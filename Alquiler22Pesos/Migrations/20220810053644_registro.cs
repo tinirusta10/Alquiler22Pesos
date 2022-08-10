ef92129376e56c04c5fa1db0591f13f73fff453e
IsaacVGutierrez <90696341+IsaacVGutierrez@users.noreply.github.com>
2022-08-10 11:21:54 -0300

    migracion

---
+++ Alquiler22Pesos/Migrations/20220810053644_registro.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Alquiler22Pesos.Migrations
+{
+    public partial class registro : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
